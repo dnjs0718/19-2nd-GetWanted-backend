3738216b699be48c213b2d488226c0ee34aaffe1
viva-kite <kite@adoc.co.kr>
2021-08-11 14:12:16 +0900

    travis ci test

--- companies/views.py
+++ companies/views.py
@@ -162,26 +162,3 @@ class NotificationLikeView(APIView):
             return JsonResponse({'MESSAGE': "KEY_ERROR"}, status = 400)
         except ValueError:
             return JsonResponse({'MESSAGE': "VALUE_ERROR"}, status = 400)
-
-
-    def excel_export(request):
-            response = HttpResponse(content_type= "application/vns.ms-excel")
-            response["Content-Disposition"] = 'attachment;filename*=UTF-8\'\'example.xls'
-            wb = xlwt.Workbook(encoding='ansi')
-            ws = wb.add_sheet('테스트테스트카이트')
-
-            row_num = 0
-            col_names = ['name', 'address']
-
-            for idx, col_name in enumerate(col_names):
-                ws.write(row_num, idx, col_name)
-
-            rows = Company.objects.all().values_list('name','address')
-
-            for row in rows:
-                row_num+=1
-                for col_num, attr in enumerate(row):
-                    ws.write(row_num, col_num, attr)
-            
-            wb.save(response)
-            return response
